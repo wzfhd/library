396fa38c4c2a2c4ddb26e47823cb40ac577fc023
huadi <20212020020@fudan.edu.cn>
2023-11-11 17:10:23 +0800

    test

--- lect1.py
+++ lect1.py
@@ -1,28 +1,22 @@
-""" Module: Test """
+import os
+import pandas as pd
 
-class S:
-    def __init__(self):
-        self.s = 1
-    
-    def print_values(self):
-        for i in range(1, 4):
-            m = i
-            print(i + 1)
-            print(m)
-            print(self.s)
-        self.s = 2
-        print(self.s)
-        print(i + 1)
-        print(m)
-        print(self.s)
-    
-    def set_m(self):
-        if self.s == 1:
-            m = 2
-        else:
-            m = 4
-        return m
+def get_file_names(directory_path):
+    files = os.listdir(directory_path)
+    for file in files:
+        first_file_name = file
+        print(first_file_name)
+        #print(file)
 
-s = S()
-s.print_values()
-m = s.set_m()
+def read_excel_file(file_path):
+    data = pd.read_excel(file_path)
+    s = data.iloc[2,0]
+    #print(data)
+    print(s)
+
+if __name__ == "__main__":
+    directory_path = "D:/BaiduNetdisk"  # 替换为你想要读取文件名的目录路径
+    get_file_names(directory_path)
+
+    excel_file_path = 'C:/Users/Windows11/Desktop/emu会议纪要.xlsx'
+    read_excel_file(excel_file_path)
