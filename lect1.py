1a8473131dd2e679d9c31351445aa3cfd9bf8d50
huadi <20212020020@fudan.edu.cn>
2023-11-11 10:37:09 +0800

    test

--- lect1.py
+++ lect1.py
@@ -1,2 +1,19 @@
-for i in 1:2
-print('%d',t)
+
+s=1
+for i in range(1,4):
+    m=i
+    print(i+1)
+    print(m)
+    print(s)
+s=2
+print(s)
+print(i+1)
+print(m)
+print(s)
+
+if s == 1:
+    m=2
+else:
+    m=4
+
+
